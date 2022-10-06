3a79d1530ba01be2e6d884b6944b1d4c28af39f3
torinunna <torinunna9588@gmail.com>
2022-10-06 16:33:55 +0900

    delete useless properties and implement bind method to HomeVC

--- CarrotHomeTab/Home/HomeViewController.swift
+++ CarrotHomeTab/Home/HomeViewController.swift
@@ -6,20 +6,31 @@
 //
 
 import UIKit
+import Combine
 
 class HomeViewController: UIViewController {
+    
+    let viewModel: HomeViewModel = HomeViewModel(network: NetworkService(configuration: .default))
+    var subscriptions = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        viewModel.fetch()
     }
-
-    @IBAction func ctaButtonPressed(_ sender: UIButton) {
-        let sb = UIStoryboard(name: "Detail", bundle: nil)
+    
+    private func bind() {
+        viewModel.$items
+            .receive(on: RunLoop.main)
+            .sink { items in
+                print("--> update collection view \(items)")
+            }.store(in: &subscriptions)
         
-        let vc = sb.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-        
-        navigationController?.pushViewController(vc, animated: true)
-        
+        viewModel.itemPressed
+            .sink { item in
+                let sb = UIStoryboard(name: "Detail", bundle: nil)
+                let vc = sb.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+                self.navigationController?.pushViewController(vc, animated: true)
+            }.store(in: &subscriptions)
     }
+   
 }
